7ce5b44b051a54d60073f30f0ba27e72c610c7e3
Ferran <fcastane@watchity.com>
2019-02-23 19:42:47 +0100

    Delete all unused code.

--- js/LocalStorage.js
+++ js/LocalStorage.js
@@ -1,7 +1,7 @@
 var emmagatzematge = {
     taula: document.getElementById('taula'),
     desar: function (key, value) {
-        localStorage.setItem(document.getElementById('nom').value, value);
+        localStorage.setItem(document.getElementById('inputUsername').value, value);
         emmagatzematge.esborrarTaula();
         emmagatzematge.mostrar();
     },
@@ -9,13 +9,16 @@ var emmagatzematge = {
         for (var i = 0; i < localStorage.length; i++) {
             var fila = taula.insertRow(0);
             fila.insertCell(0).innerHTML = localStorage.key(i);
+            //fila.insertCell(0).innerHTML = localStorage.getItem(localStorage.key(i));
         };
     },
    esborrarTaula: function() {
         while (taula.rows.length > 0) {
             taula.deleteRow(0);
         }
-    }
+    },
+
 }
-document.getElementById('desar').addEventListener('click', emmagatzematge.desar, false);
+document.getElementById('startGame').addEventListener('click', emmagatzematge.desar, false);
+
 emmagatzematge.mostrar();
